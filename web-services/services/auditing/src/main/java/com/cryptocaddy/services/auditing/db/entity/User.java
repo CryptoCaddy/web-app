a49ce34e0e0157ef3286610cbdb419ff59c6e68d
Saswat Bhattacharya <sas.saswat@gmail.com>
2018-02-01 21:16:16 -0800

    Fixed so server is able to start

--- web-services/services/auditing/src/main/java/com/cryptocaddy/services/auditing/db/entity/User.java
+++ web-services/services/auditing/src/main/java/com/cryptocaddy/services/auditing/db/entity/User.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
