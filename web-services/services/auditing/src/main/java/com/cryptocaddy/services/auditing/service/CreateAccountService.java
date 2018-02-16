aceb4c85d2b6feb629cfa0b88b6d616405fcdd03
Jon Waggoner <jwaggs27@gmail.com>
2018-02-16 01:47:45 -0500

    added jwt info to create account body

--- web-services/services/auditing/src/main/java/com/cryptocaddy/services/auditing/service/CreateAccountService.java
+++ web-services/services/auditing/src/main/java/com/cryptocaddy/services/auditing/service/CreateAccountService.java
@@ -2,6 +2,8 @@ package com.cryptocaddy.services.auditing.service;
 
 import com.cryptocaddy.services.auditing.model.Result;
 import com.cryptocaddy.services.auditing.model.request.RequestCreateAccount;
+import com.cryptocaddy.services.common.authentication.JWTAuthenticator;
+import com.cryptocaddy.services.common.authentication.JWTBody;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +11,7 @@ public class CreateAccountService {
 
     public Result createAccount(RequestCreateAccount requestCreateAccount) {
         // TODO: 1/13/2018 - Wire up to some core method that will add this user to the database
+        JWTBody jwtBody = JWTAuthenticator.getBodyFromToken(requestCreateAccount.getToken());
 
         return new Result("Success");
     }
