1852f91df001904056b87f81247bdb6b559f4b09
Jon Waggoner <jwaggs27@gmail.com>
2018-02-28 10:13:26 -0500

    disable csrf protection

--- web-services/libraries/rest-config/src/main/java/com/cryptocaddy/services/common/security/SecurityConfig.java
+++ web-services/libraries/rest-config/src/main/java/com/cryptocaddy/services/common/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
